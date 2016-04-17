4e8f1a2b95a9e0337ff405a2484cb8a72c0a205a
zhangyu94 <463568697@qq.com>
2016-04-17 15:29:42 +0800

    btn added

--- js/views/histogram-panel.view.js
+++ js/views/histogram-panel.view.js
@@ -27,6 +27,61 @@ define([
 		initialize: function(options){
 			var self = this;
 			var model = self.model;
+			console.log("reach!!!!")
+
+
+
+			// click on sort buttons
+			$("#histogram-panel .sort-btn").click(function() {
+				$("#innerTopLeft .sort-btn").removeClass("active");
+				$(this).addClass("active");
+				sortMode = $(this).attr("sort-type");
+				//sortMode为"time"或"size"
+				
+				console.log("click!!")
+				//drawHistogram(choose_displayArray(sortMode,datadimMode),datadimMode);
+			});
+
+			// click on 按树的数值决定高度与树的结点数决定高度之间切换 的 按钮
+			$("#histogram-panel .datadim-btn").click(function() {
+				$("#innerTopLeft .datadim-btn").removeClass("active");
+				$(this).addClass("active");
+				datadimMode = $(this).attr("datadim-type");
+				//datadimMode为"flowsize"或"nodenum"
+
+				console.log("click!!")
+				//drawHistogram(choose_displayArray(sortMode,datadimMode),datadimMode);
+			});
+
+			$('#histogram-panel #help').click(function(){
+				console.log("!!!")
+				$("#dialog-confirm").dialog("open");
+			});
+			$("#histogram-panel #dialog-confirm").dialog
+			({
+				width:"900",
+				height:"600",
+		        //modal: true,             // 创建模式对话框
+		        autoOpen: false,         // 只初始化，不显示
+		        buttons: {
+			        "Back": function() {
+
+			        	d3.selectAll(".help_img").remove()
+			        	$( ".help_document" ).append( 
+			        		"<img src=\"helpdocument/signal_tree_interface_intro.png\" class=\"help_img\" width=\"800px\" >"
+			        		);
+			        },
+			        "Next": function() {
+			        	d3.selectAll(".help_img").remove()
+			        	$( ".help_document" ).append( 
+			        		"<img src=\"helpdocument/signal_tree.png\" class=\"help_img\" width=\"550px\" >"
+			        		);
+			        }
+			    }
+
+		    });
+
+
 
 
 
---
+++ templates/histogramViewPanel.tpl
@@ -0,0 +1,18 @@
+<span class="btn-group" role="group">
+    <span class="datadim-btn btn btn-default btn-xs active" datadim-type="flowsize">
+        <!--btn-default和btn-xs都控制了btn的样式-->
+        <span class="glyphicon glyphicon-sort"></span>
+    </span>
+    <span class="datadim-btn btn btn-default btn-xs" datadim-type="nodenum">
+        <span class="glyphicon glyphicon-th"></span>
+    </span>
+    <span class="sort-btn btn btn-default btn-xs active" sort-type="time">
+        <span class="glyphicon glyphicon-time"></span>
+    </span>
+    <span class="sort-btn btn btn-default btn-xs" sort-type="size">  
+        <span class="glyphicon glyphicon-signal"></span>
+    </span>
+    <span class="btn btn-default btn-xs" id="help">  
+        <span class="glyphicon glyphicon-exclamation-sign"></span>
+    </span>
+</span>
